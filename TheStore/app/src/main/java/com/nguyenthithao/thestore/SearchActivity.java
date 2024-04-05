06a4fcebca886bfde0ea474dad9bb7b911e63088
thaoong <thao4985@gmail.com>
2024-04-06 00:34:18 +0700

    update search

--- TheStore/app/src/main/java/com/nguyenthithao/thestore/SearchActivity.java
+++ TheStore/app/src/main/java/com/nguyenthithao/thestore/SearchActivity.java
@@ -13,83 +13,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.nguyenthithao.thestore.databinding.ActivitySearchBinding;
+
 public class SearchActivity extends AppCompatActivity {
-    
-    ImageView icSearch;
-    EditText searchBar;
-    Button btnSearch;
+    ActivitySearchBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        //setContentView(R.layout.activity_search);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.edtSearch.requestFocus();
+        hideActionBar();
+        addEvents();
+    }
 
-        displayActionBar();
-        setHasOptionsMenu(true);
-        addViews();
-        icSearch = findViewById(R.id.icSearch);
-        searchBar = findViewById(R.id.searchBar);
-        btnSearch = findViewById(R.id.btnSearch);
+    private void hideActionBar() {
+        getSupportActionBar().hide();
+    }
 
-        icSearch.setOnClickListener(new View.OnClickListener() {
+    private void addEvents() {
+        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showSearchBar();
-            }
-        });
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String query = searchBar.getText().toString();
+                String query = binding.edtSearch.getText().toString();
                 performSearch(query);
             }
         });
-    }
 
-    public void showSearchBar() {
-        icSearch.setVisibility(View.GONE);
-        searchBar.setVisibility(View.VISIBLE);
-        btnSearch.setVisibility(View.VISIBLE);
-    }
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
-    public void hideSearchBar() {
-        icSearch.setVisibility(View.VISIBLE);
-        searchBar.setVisibility(View.GONE);
-        btnSearch.setVisibility(View.GONE);
+        binding.btnEmptySearchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.edtSearch.setText("");
+            }
+        });
     }
 
     private void performSearch(String query) {
         // Xử lý tìm kiếm
-    }
-
-
-    private void addViews() {
-
-    }
-
-    private void setHasOptionsMenu(boolean b) {
-    }
-
-    private void displayActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_ios_24);
-        actionBar.setTitle(Html.fromHtml("<font color='#5C3507'>Tìm kiếm</font>"));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
     }
 }
