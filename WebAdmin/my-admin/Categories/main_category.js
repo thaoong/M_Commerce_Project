d144418ad65fe443ef5f443cca5a75c82978e883
Tquyh <quynhpvt21411c@st.uel.edu.vn>
2024-04-23 22:51:01 +0700

    Update web

--- WebAdmin/my-admin/Categories/main_category.js
+++ WebAdmin/my-admin/Categories/main_category.js
@@ -1,42 +1,73 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('/Volumes/Study/Android_Project/WebAdmin/my-admin/config/serviceAccountKey.json');
+document.addEventListener("DOMContentLoaded", () => {
+  let tableBody = document.querySelector("tbody");
+  let addCategory = document.querySelector(".add-category");
+  let popup = document.querySelector(".popup");
+  let form = document.querySelector("form");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://console.firebase.google.com/u/4/project/thestore-55f0f/database/thestore-55f0f-default-rtdb/data/~2F'
-});
-// Get a reference to the database service
-const database = admin.database();
-const categoriesRef = database.ref('categories/');
+  // Get a reference to the database service
+  var database = firebase.database();
+  var categoriesRef = firebase.database().ref("categories/");
 
-// Write data
-function writeCategoriesData(id, imageLink, name) {
-  const newCategoryRef = categoriesRef.push();
-  newCategoryRef.set({
-    id: id,
-    imageLink: imageLink,
-    name: name
-  })
-  .then(() => {
-    console.log("Data written successfully.");
-  })
-  .catch((error) => {
-    console.log("Error writing data:", error);
-  });
-}
-
-// Example usage
-writeCategoriesData("category10", "111", "234");
-
-// Read data
-categoriesRef.on('value', (snapshot) => {
-  const categories = snapshot.val();
-  console.log(categories);
-
-  // Iterate over the categories object
-  for (const categoryKey in categories) {
-    const category = categories[categoryKey];
-    console.log(category);
-    console.log(typeof category);
+  // Write data
+  function writeCategoriesData(id, imageLink, name) {
+    categoriesRef
+      .child(id)
+      .set({
+        id: id,
+        imageLink: imageLink,
+        name: name,
+      })
+      .then(
+        (onFullFilled) => {
+          console.log("Data written successfully.");
+        },
+        (onRejected) => {
+          console.log("Error writing data:", onRejected);
+        }
+      );
   }
+
+  // Example usage
+  writeCategoriesData("category10", "111", "234");
+
+  // Read data
+  categoriesRef.on("value", (snapshot) => {
+    const categories = snapshot.val();
+    console.log(categories);
+
+    // Clear the table body
+    tableBody.innerHTML = "";
+
+    // Iterate over the categories object
+    for (category in categories) {
+      let tr = `
+      <tr>
+        <td>${categories[category].id}</td>
+        <td>${categories[category].imageLink}</td>
+        <td>${categories[category].name}</td>
+        <td>
+          <button class="edit">Edit</button>
+          <button class="delete">Delete</button>
+        </td>
+      </tr>
+    `;
+      tableBody.innerHTML += tr;
+    }
+  });
+
+  // Write Dynamic Data
+  addCategory.addEventListener("click", () => {
+    popup.classList.add("active");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      writeCategoriesData(form.id.value, form.imageLink.value, form.name.value);
+    });
+  });
+
+  // Close popup
+  window.addEventListener("click", (e) => {
+    if (e.target == popup) {
+      popup.classList.remove("active");
+    }
+  });
 });
