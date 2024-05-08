ec75516563c947099c624ab0d0a1637b81ecab86
thaoong <thao4985@gmail.com>
2024-05-08 23:35:50 +0700

    update order management

--- WebAdmin/my-admin/JS/order_detail.js
+++ WebAdmin/my-admin/JS/order_detail.js
@@ -122,8 +122,20 @@ function cancelOrder() {
 }
 
 function updateComplete() {
-    const orderStatusRef = ref(db, `orders/${key}/status`);
-    set(orderStatusRef, "Hoàn tất")
+    const orderRef = ref(db, `orders/${key}`);
+    
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    const formattedDate = `${day}/${month}/${year}`;
+
+    const updateData = {
+        status: "Hoàn tất",
+        receivedDate: formattedDate
+    };
+
+    update(orderRef, updateData)
         .then(() => {
             alert("Update order status successfully");
             window.history.back();
