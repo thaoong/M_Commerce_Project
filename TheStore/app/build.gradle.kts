a2c0d07c744e203d0c7c7ddff7442d49dac13171
thaoong <thao4985@gmail.com>
2024-05-19 23:33:11 +0700

    update

--- TheStore/app/build.gradle.kts
+++ TheStore/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.nguyenthithao.thestore"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
