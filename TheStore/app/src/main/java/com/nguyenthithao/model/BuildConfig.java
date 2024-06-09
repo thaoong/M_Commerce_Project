54308276b37a7a0162b6267206147681069049c4
Thao Nguyen <95263116+thaoong@users.noreply.github.com>
2024-06-09 21:31:14 +0700

    Update BuildConfig.java

--- TheStore/app/src/main/java/com/nguyenthithao/model/BuildConfig.java
+++ TheStore/app/src/main/java/com/nguyenthithao/model/BuildConfig.java
@@ -1,5 +1,5 @@
 package com.nguyenthithao.model;
 
 public class BuildConfig {
-    public static String apiKey = "AIzaSyB6IPrLBVATMcA5I8rri9q1EKYXNq20YOc";
+    public static String apiKey = "{Get an API Key at https://aistudio.google.com/app/apikey and replace here}";
 }
